f7d3be4f54c703eb433724c7f8ed61d809e79499
Guilherme-Melo <guilherme.melo@ibm.com>
2020-05-02 16:25:27 -0300

    adding the dope font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
